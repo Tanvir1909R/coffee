6e7c94b2a2fe5d0d52a8f6569f78cc1e4c13457f
Tanvir1909R <tanvirreshad112@gmail.com>
2022-04-05 16:31:16 +0600

    first commit

--- src/components/Reserv.tsx
+++ src/components/Reserv.tsx
@@ -8,7 +8,20 @@ function Reserv() {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa enim. Nullam id varius nunc.</p>
                 <h4>CALL US: 1-800-1234-5678</h4>
             </div>
-            <div className="opentime"></div>
+            <div className="opentime">
+              <div className="date">
+                <h1>MON-FRI</h1>
+                <p>10:00 AM</p>
+                <p>To</p>
+                <p>8:00 PM</p>
+              </div>
+              <div className="date">
+                <h1>SAT-SUN</h1>
+                <p>10:00 AM</p>
+                <p>To</p>
+                <p>8:00 PM</p>
+              </div>
+            </div>
         </div>
     </section>
   )
