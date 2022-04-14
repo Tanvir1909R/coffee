3fde00a3483eb3b2103ab6f698c3e630b4878856
Tanvir1909R <tanvirreshad112@gmail.com>
2022-04-14 15:25:12 +0600

    commit

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,16 +1,37 @@
-
+import menuItem from "./../data/menuItem.json";
 
 function Menu() {
   return (
     <section className="menu_sec">
-        <div className="menu_sec_Wrapper">
-            <div className="title">
-                <h1>Our Menu</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa enim.az</p>
-            </div>
+      <div className="menu_sec_Wrapper">
+        <div className="title">
+          <h1>Our Menu</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            blandit massa enim.az
+          </p>
         </div>
+        <div className="items">
+          {menuItem.map((item, index) => {
+            return (
+              <div className="item" key={index}>
+                <div className="item_title">
+                  <h4> {item.item} </h4>
+                  <div className="line"></div>
+                  <h4> {item.price} </h4>
+                </div>
+                <div className="item_dec">
+                  <p>
+                    {item.dec}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
