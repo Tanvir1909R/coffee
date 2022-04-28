8270169688a5c0f2971ce616ff3613135bd2fdbd
Tanvir1909R <tanvirreshad112@gmail.com>
2022-04-28 22:34:50 +0600

    commit

--- src/components/NewsEvents.tsx
+++ src/components/NewsEvents.tsx
@@ -1,18 +1,32 @@
-
+import newsData from "../data/news.json";
 function NewsEvents() {
   return (
     <section className="newsEvents_wrapper">
-        <div className="news_wrapper">
+      <div className="news_wrapper">
         <div className="title">
-          <h1>Our Menu</h1>
+          <h1>News & Events</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
             blandit massa enim.az
           </p>
         </div>
+        <div className="news_feed">
+          {newsData.map((item, indx) => {
+            return (
+              <div className="news">
+                <h1>{item.title}</h1>
+                <h4>{item.date}</h4>
+                <p>
+                  {item.dec}
+                </p>
+                <h4>{item.persion}</h4>
+              </div>
+            );
+          })}
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default NewsEvents
+export default NewsEvents;
