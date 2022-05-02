9820390582d1be9f41710e240db6ab0c56d97a82
Tanvir1909R <tanvirreshad112@gmail.com>
2022-05-02 23:09:26 +0600

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,28 @@
-
 function Footer() {
   return (
     <section className="footer">
-        <div className="footer_wrapper">
-            <div className="logo2">
-                <img src="./img/logo-light.png" alt="" />
-            </div>
-            <div className="location">
-                <h1>NEW YORK</h1>
-                <p>123, New York Street, New York (123) 456-7890</p>
-                <div className="social">
-                    
-                </div>
-            </div>
+      <div className="footer_wrapper">
+        <div className="logo2">
+          <img src="./img/logo-light.png" alt="" />
         </div>
+        <div className="location">
+          <h1>NEW YORK</h1>
+          <p>123, New York Street, New York (123) 456-7890</p>
+          <div className="social"></div>
+        </div>
+        <div className="location">
+          <h1>FRESNO</h1>
+          <p>123, Fresno Street, Fresno (123) 456-7890</p>
+          <div className="social"></div>
+        </div>
+        <div className="location">
+          <h1>BIRMINGHAM</h1>
+          <p>123, Birmingham Street, Birmingham (123) 456-7890</p>
+          <div className="social"></div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
